93e8238abee8fb5ffbbcd8e37aa68155615c4926
Axel Vaganay <avaganay@z2r8p8.42lyon.fr>
2023-06-15 12:47:38 +0200

    start move with printf w a s d

--- header/typedef.h
+++ header/typedef.h
@@ -6,12 +6,19 @@
 /*   By: avaganay <avaganay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 14:12:47 by maujogue          #+#    #+#             */
-/*   Updated: 2023/06/15 11:22:20 by avaganay         ###   ########.fr       */
+/*   Updated: 2023/06/15 12:42:03 by avaganay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TYPEDEF_H
 # define TYPEDEF_H
+
+# define W_KEY 119
+# define A_KEY 97
+# define S_KEY 115
+# define D_KEY 100
+# define ESC_KEY 65307
+# define SPACE_KEY 32
 
 typedef struct	s_data
 {
