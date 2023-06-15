9a2f047b7ab8781463481ea6676d22f1529dcca2
Axel Vaganay <avaganay@z2r8p8.42lyon.fr>
2023-06-15 12:37:25 +0200

    clean mini map + separete init and ft_mlx

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: avaganay <avaganay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 13:54:47 by maujogue          #+#    #+#             */
-/*   Updated: 2023/06/15 11:41:21 by avaganay         ###   ########.fr       */
+/*   Updated: 2023/06/15 12:36:47 by avaganay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int main(int argc, char **argv)
     (void)argc;
     (void)argv;
     init_cub(&all);
+    ft_mlx(&all);
     free_exit(&all);
     return (0);
 }
--- src/mlx.c
+++ src/mlx.c
@@ -6,7 +6,7 @@
 /*   By: avaganay <avaganay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 15:33:07 by avaganay          #+#    #+#             */
-/*   Updated: 2023/06/15 11:33:57 by avaganay         ###   ########.fr       */
+/*   Updated: 2023/06/15 12:36:34 by avaganay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@
 
 void    ft_mlx(t_all *all)
 {
-    draw_map(all);
     check_keypress(all);
+    draw_map(all);
+    mlx_put_image_to_window(all->mlx, all->mlx_win, all->data->img, 0, 0);
     mlx_loop(all->mlx);
 }
