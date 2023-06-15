d1bac3a382f75ecd2d40ced10497dd6c1fc13673
Mathis Aujogue <maujogue@z1r5p1.42lyon.fr>
2023-06-15 16:44:04 +0200

    merge ancient

--- src/main.c
+++ src/main.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: avaganay <avaganay@student.42.fr>          +#+  +:+       +#+        */
+/*   By: maujogue <maujogue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 13:54:47 by maujogue          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/06/15 12:46:11 by maujogue         ###   ########.fr       */
-=======
-/*   Updated: 2023/06/15 11:18:53 by avaganay         ###   ########.fr       */
->>>>>>> Axel
+/*   Updated: 2023/06/15 16:43:12 by maujogue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +16,12 @@ int	main(int argc, char **argv)
 {
 	t_all	all;
 
-<<<<<<< HEAD
 	if (argc == 2)
 	{
 		init_cub(&all, argv[1]);
-		check_keypress(&all);
+		ft_mlx(&all);
 		mlx_loop(all.mlx);
 		free_exit(&all, 0, "");
 	}
 	return (0);
-=======
-    (void)argc;
-    (void)argv;
-    init_cub(&all);
-    ft_mlx(&all);
-    free_exit(&all);
-    return (0);
->>>>>>> Axel
 }
