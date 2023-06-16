09e077add4cb2c60c7d942b552dde45884fa2b7b
Axel Vaganay <avaganay@z2r7p6.42lyon.fr>
2023-06-16 10:58:31 +0200

    fusion parsing and minimap work

--- src/raycasting/raycasting.c
+++ src/raycasting/raycasting.c
@@ -6,23 +6,23 @@
 /*   By: avaganay <avaganay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 14:56:12 by avaganay          #+#    #+#             */
-/*   Updated: 2023/06/16 10:48:22 by avaganay         ###   ########.fr       */
+/*   Updated: 2023/06/16 10:57:58 by avaganay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header/cub.h"
 
-double	ft_sqrt(double x)
-{
-	double	guess;
-	double	epsilon;
+// double	ft_sqrt(double x)
+// {
+// 	double	guess;
+// 	double	epsilon;
 
-	guess = 1.0;
-	epsilon = 0.00001;
-	while (guess * guess - x > epsilon || x - guess * guess > epsilon)
-		guess = (guess + x / guess) / 2.0;
-	return (guess);
-}
+// 	guess = 1.0;
+// 	epsilon = 0.00001;
+// 	while (guess * guess - x > epsilon || x - guess * guess > epsilon)
+// 		guess = (guess + x / guess) / 2.0;
+// 	return (guess);
+// }
 
 int	raycasting(t_all *all)
 {
@@ -36,8 +36,8 @@ int	raycasting(t_all *all)
       all->ray->raydir.y = all->ray->dir.y + all->ray->plane.y * all->ray->cameraX;
       all->ray->mapX = (int)all->ray->pos.x;
       all->ray->mapY = (int)all->ray->pos.y;
-      all->ray->deltadist.x = ft_sqrt(1 + (all->ray->raydir.y * all->ray->raydir.y) / (all->ray->raydir.y * all->ray->raydir.y));
-      all->ray->deltadist.y = ft_sqrt(1 + (all->ray->raydir.x * all->ray->raydir.x) / (all->ray->raydir.y * all->ray->raydir.y));
+      all->ray->deltadist.x = sqrt(1 + (all->ray->raydir.y * all->ray->raydir.y) / (all->ray->raydir.y * all->ray->raydir.y));
+      all->ray->deltadist.y = sqrt(1 + (all->ray->raydir.x * all->ray->raydir.x) / (all->ray->raydir.y * all->ray->raydir.y));
 		i++;
 	}
 	return (0);
