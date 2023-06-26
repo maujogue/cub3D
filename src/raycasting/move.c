6106d4d9e3b3e2d17fa9e0a151128f7da040aefb
Mathis Aujogue <maujogue@z2r6p6.42lyon.fr>
2023-06-26 12:10:19 +0200

    colision

--- src/raycasting/move.c
+++ src/raycasting/move.c
@@ -6,49 +6,55 @@
 /*   By: maujogue <maujogue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 12:06:42 by maujogue          #+#    #+#             */
-/*   Updated: 2023/06/23 17:53:17 by maujogue         ###   ########.fr       */
+/*   Updated: 2023/06/26 11:27:05 by maujogue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header/cub.h"
 
-void	move_up_down(t_all *all, double move_speed)
+void	move_up_down(t_all *all)
 {
 	int	x;
 	int	y;
 
-	x = (int)floor(all->ray.p_pos.x);
-	if (all->keys[0] == 0)
+	x = (int)floor(all->ray.p_pos.x + cos(all->ray.angle) / 10);
+	y = (int)floor(all->ray.p_pos.y + sin(all->ray.angle) / 10);
+	if (all->keys[0] == 0 && all->pars.map[y][x] != '1')
 	{
-		y = (int)floor(all->ray.p_pos.y - move_speed);
-		if (all->pars.map[y][x] != '1')
-			all->ray.p_pos.y -= move_speed;
+		all->ray.p_pos.x += cos(all->ray.angle) / 10;
+		all->ray.p_pos.y += sin(all->ray.angle) / 10;
 	}
-	if (all->keys[1] == 0)
+	if (all->keys[1] == 0 && all->pars.map[y][x] != '1')
 	{
-		y = (int)floor(all->ray.p_pos.y + move_speed);
-		if (all->pars.map[y][x] != '1')
-			all->ray.p_pos.y += move_speed;
+		all->ray.p_pos.x -= cos(all->ray.angle) / 10;
+		all->ray.p_pos.y -= sin(all->ray.angle) / 10;
 	}
 }
 
-void	move_left_right(t_all *all, double move_speed)
+void	move_left_right(t_all *all)
 {
 	int	x;
 	int	y;
 
-	y = (int)floor(all->ray.p_pos.y);
 	if (all->keys[2] == 0)
 	{
-		x = (int)floor(all->ray.p_pos.x - move_speed);
+		x = (int)floor(all->ray.p_pos.x + cos(all->ray.angle - PI / 2) / 10);
+		y = (int)floor(all->ray.p_pos.y + sin(all->ray.angle - PI / 2) / 10);
 		if (all->pars.map[y][x] != '1')
-			all->ray.p_pos.x -= move_speed;
+		{
+			all->ray.p_pos.y += sin(all->ray.angle - PI / 2) / 10;
+			all->ray.p_pos.x += cos(all->ray.angle - PI / 2) / 10;
+		}
 	}
 	if (all->keys[3] == 0)
 	{
-		x = (int)floor(all->ray.p_pos.x + move_speed);
+		x = (int)floor(all->ray.p_pos.x + cos(all->ray.angle + PI / 2) / 10);
+		y = (int)floor(all->ray.p_pos.y + sin(all->ray.angle + PI / 2) / 10);
 		if (all->pars.map[y][x] != '1')
-			all->ray.p_pos.x += move_speed;
+		{
+			all->ray.p_pos.y += sin(all->ray.angle + PI / 2) / 10;
+			all->ray.p_pos.x += cos(all->ray.angle + PI / 2) / 10;
+		}
 	}
 }
 
@@ -75,15 +81,15 @@ void	rotate_left_right(t_all *all)
 int	move_player(t_all *all)
 {
 	double	move_speed;
-
+	
 	move_speed = 0.05;
 	if ((all->keys[0] == 0 && all->keys[2] == 0)
 		|| (all->keys[0] == 0 && all->keys[3] == 0)
 		|| (all->keys[1] == 0 && all->keys[2] == 0)
 		|| (all->keys[1] == 0 && all->keys[3] == 0))
 		move_speed /= sqrt(2);
-	move_up_down(all, move_speed);
-	move_left_right(all, move_speed);
+	move_up_down(all);
+	move_left_right(all);
 	rotate_left_right(all);
 	return (0);
 }
